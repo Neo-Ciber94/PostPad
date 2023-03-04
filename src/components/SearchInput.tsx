6b2fb910c305724839ec2de5d24f131ac159cc9d
Freddy L <neo-ciber_94@hotmail.com>
2023-03-04 16:50:37 -0400

    Added search input

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,3 +1,23 @@
-export default function SearchInput() {
-  return <input type="search" />;
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+
+export type SearchInputProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
+
+export default function SearchInput(props: SearchInputProps) {
+  return (
+    <div className="relative">
+      <input
+        {...props}
+        type="search"
+        placeholder="Search..."
+        className="h-10 w-full rounded-lg bg-white pr-4
+            pl-14 text-black outline-none shadow-md shadow-gray-800"
+      />
+      <div className="absolute top-2 left-4 cursor-pointer">
+        <MagnifyingGlassIcon className="h-6 w-6 text-gray-600 opacity-80" />
+      </div>
+    </div>
+  );
 }
--- src/components/pages/notes/NotesListPageBase.tsx
+++ src/components/pages/notes/NotesListPageBase.tsx
@@ -1,3 +1,4 @@
+import SearchInput from "@/components/SearchInput";
 import { Note } from "@/lib/schemas/Note";
 import Link from "next/link";
 import Button from "../../Button";
@@ -7,15 +8,22 @@ export interface NoteListPageBase {
   initialNotes: Note[];
 }
 
-export default function NotesListPageBase({ initialNotes: notes }: NoteListPageBase) {
+export default function NotesListPageBase({
+  initialNotes: notes,
+}: NoteListPageBase) {
   return (
     <div className="p-2 dark:text-white">
-      <div className="flex flex-row justify-center py-2 px-10 md:px-[10%] lg:px-[25%]">
+      <div className="py-2 px-10 md:px-[10%] lg:px-[25%]">
         <Link href="/notes/new" className="w-full  pb-5">
           <Button className="flex w-full flex-row items-center justify-center gap-4">
             New Note
           </Button>
         </Link>
+
+        <hr className="my-8 bg-gray-400 opacity-10"/>
+        <div className="mt-4 mb-2">
+          <SearchInput />
+        </div>
       </div>
 
       <div className="px-10 md:px-[10%] lg:px-[20%]">
