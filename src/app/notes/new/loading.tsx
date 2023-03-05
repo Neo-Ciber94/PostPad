587b38204de10e8c5e2296b0d6bd15f3845248e3
Freddy L <neo-ciber_94@hotmail.com>
2023-03-04 22:18:47 -0400

    minor changes

--- src/app/notes/new/loading.tsx
+++ src/app/notes/new/loading.tsx
@@ -1,3 +1,9 @@
+import NoteFormSkeleton from "@/components/NoteFormSkeleton";
+
 export default function Loading() {
-  return "Loading...";
+  return (
+    <div className="p-4">
+      <NoteFormSkeleton />
+    </div>
+  );
 }
