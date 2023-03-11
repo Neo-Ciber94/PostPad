6ee4282fa5ec52100cf0d238d7bc19c22cb857aa
Freddy L <neo-ciber_94@hotmail.com>
2023-03-10 21:59:09 -0400

    Updated skeleton

--- src/components/MarkdownEditorLoading.tsx
+++ src/components/MarkdownEditorLoading.tsx
@@ -1,6 +1,6 @@
 export default function MarkdownEditorLoading() {
   return (
-    <div className="h-[300px] animate-pulse rounded-md bg-slate-500">
+    <div className="h-[300px] animate-pulse rounded-md bg-violet-300/30">
       {/* Markdown Editor */}
     </div>
   );
