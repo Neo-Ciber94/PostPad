336536a888662d6a1005799a3d05b3597c3f65ce
Freddy L <neo-ciber_94@hotmail.com>
2023-03-23 23:32:48 -0400

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @next/next/no-before-interactive-script-outside-document */
 import "./styles/globals.scss";
 import Main from "@/components/Main";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { DarkModeProvider } from "@/lib/client/contexts/DarkModeContext";
 import { getUserPrefersDarkMode } from "@/lib/server/utils/getUserPrefersDarkMode";
+import Script from "next/script";
 
 export const metadata = {
   title: "PostPad",
@@ -29,6 +31,14 @@ export default async function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/monokai-sublime.min.css"
+        />
+        <Script
+          strategy="beforeInteractive"
+          src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"
+        />
       </head>
       <body
         className="bg-base-500 scrollbar
--- src/components/Editor/HighLight.tsx
+++ src/components/Editor/HighLight.tsx
@@ -5,14 +5,7 @@ import Script from "next/script";
 export function HightLight() {
   return (
     <>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/monokai-sublime.min.css"
-      />
-      <Script
-        strategy="beforeInteractive"
-        src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"
-      />
+
     </>
   );
 }
