3c2523ea6c2829252b3321c997cd17eeb057477a
Freddy L <neo-ciber_94@hotmail.com>
2023-03-27 01:25:37 -0400

    Added border to dark mode toolbar

--- src/components/editor/PostEditor.tsx
+++ src/components/editor/PostEditor.tsx
@@ -2,32 +2,23 @@ import "react-quill/dist/quill.snow.css";
 import hljs from "highlight.js";
 import { useState } from "react";
 import React from "react";
-import ReactQuill from "react-quill";
+import ReactQuill, { Quill } from "react-quill";
 import { useDarkMode } from "@/lib/client/contexts/DarkModeContext";
-import {
-  ArrowsPointingInIcon,
-  ArrowsPointingOutIcon,
-} from "@heroicons/react/24/solid";
+import { ArrowsPointingInIcon, ArrowsPointingOutIcon } from "@heroicons/react/24/solid";
 import { DarkModeToggle } from "../DarkModeToggle";
 import BlotFormatter from "quill-blot-formatter";
 import CustomImage from "./CustomImage";
 import CustomVideo from "./CustomVideo";
+import QuillImageDropAndPaste from "quill-image-drop-and-paste";
 
-ReactQuill.Quill.register("modules/blotFormatter", BlotFormatter, true);
-ReactQuill.Quill.register("formats/image", CustomImage, true);
-ReactQuill.Quill.register("formats/video", CustomVideo, true);
+Quill.register("modules/blotFormatter", BlotFormatter, true);
+Quill.register("formats/image", CustomImage, true);
+Quill.register("formats/video", CustomVideo, true);
+Quill.register("modules/imageDropAndPaste", QuillImageDropAndPaste);
 
 const Font = ReactQuill.Quill.import("formats/font");
 
-Font.whitelist = [
-  "arial",
-  "courier-new",
-  "georgia",
-  "helvetica",
-  "lucida",
-  "roboto",
-  "quicksand",
-];
+Font.whitelist = ["arial", "courier-new", "georgia", "helvetica", "lucida", "roboto", "quicksand"];
 
 ReactQuill.Quill.register(Font, true);
 
@@ -36,6 +27,7 @@ const modules = {
     highlight: (text: string) => hljs.highlightAuto(text).value,
   },
   blotFormatter: {},
+  imageDropAndPaste: {},
   toolbar: [
     [{ font: Font.whitelist }],
     ["bold", "italic", "underline", "strike"],
@@ -83,11 +75,7 @@ export interface PostEditorProps {
   onChange: (value: string) => void;
 }
 
-export default function PostEditor({
-  value,
-  onChange,
-  readOnly,
-}: PostEditorProps) {
+export default function PostEditor({ value, onChange, readOnly }: PostEditorProps) {
   const { isDarkMode } = useDarkMode();
   const [isFullScreen, setIsFullScreen] = useState(false);
 
@@ -100,11 +88,6 @@ export default function PostEditor({
     scrollbar-thumb-base-900 
     `}
     >
-      {/* <Toolbar
-        isFullScreen={isFullScreen}
-        onToggleFullScreen={() => setIsFullScreen((fullScreen) => !fullScreen)}
-      /> */}
-
       <div className="flex flex-row justify-end gap-2 p-2">
         <DarkModeToggle size={24} />
         <button
