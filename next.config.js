97fbec09443abd6059f1079e8c0a848e18a0b148
Freddy L <neo-ciber_94@hotmail.com>
2023-03-31 21:57:14 -0400

    docs

--- next.config.js
+++ next.config.js
@@ -7,6 +7,13 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
   },
+  // TODO: disable for vercel and check in the github action
+  // typescript: {
+  //   ignoreBuildErrors: false,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: false,
+  // },
 };
 
 module.exports = nextConfig;
